5c4656210558bcf8fb7ad412451a560942125d95
Davi Felix <davi.felix.lima@gmail.com>
2022-03-05 21:04:47 -0300

    feature: create Post list page (no load-more functionality)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 import { GetStaticProps } from 'next';
 
+import { format } from 'date-fns';
+
+import Prismic from '@prismicio/client';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,17 +28,65 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
-  return <h1>Hello, world!</h1>;
+export default function Home({
+  postsPagination: { results, next_page },
+}: HomeProps) {
+  return (
+    <main className={`${commonStyles.container} ${styles.container}`}>
+      <ul className={styles.postList}>
+        {results.map(({ data, first_publication_date, uid }) => (
+          <li className={styles.post} key={uid}>
+            <a href="https://www.google.com" target="_blank" rel="noreferrer">
+              <h2>{data.title}</h2>
+              <h3>{data.subtitle}</h3>
+            </a>
+            <div className={styles.infoContainer}>
+              <div className={styles.info}>
+                <FiCalendar />
+                <span>{first_publication_date}</span>
+              </div>
+              <div className={styles.info}>
+                <FiUser />
+                <span>{data.author}</span>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+      {next_page && (
+        <button type="button" className={styles.nextPageButton}>
+          Carregar mais posts
+        </button>
+      )}
+    </main>
+  );
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    Prismic.Predicates.at('document.type', 'post'),
+    {
+      pageSize: 5,
+    }
+  );
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+  const { next_page, results } = postsResponse;
 
-//   // TODO
-// };
+  const formatedResults = results.map(post => {
+    const formatedDate = format(
+      new Date(post.first_publication_date),
+      'dd/MM/yyyy'
+    );
+    return {
+      ...post,
+      first_publication_date: formatedDate,
+    };
+  });
+
+  return {
+    props: {
+      postsPagination: { next_page, results: formatedResults },
+    },
+  };
+};
