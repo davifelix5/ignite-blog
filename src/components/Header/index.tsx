27783bbd65444c08944123dede961e32a84f3884
Davi Felix <davi.felix.lima@gmail.com>
2022-03-05 21:04:01 -0300

    feature: create Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,12 @@
-export default function Header() {
-  // TODO
+import Image from 'next/image';
+
+import commomStyles from '../../styles/common.module.scss';
+import styles from './header.module.scss';
+
+export function Header() {
+  return (
+    <header className={`${commomStyles.container} ${styles.headerContainer}`}>
+      <Image src="/logo.svg" width={220} height={25} />
+    </header>
+  );
 }
